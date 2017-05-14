df878301cfd2dfaed4ec160d105b52c77f39f677
Tiberiu Ichim <tibi@pixelblaster.ro>
2017-05-14 18:01:30 +0300

    Added command line options; fix port problem

--- corpus/eea_corpus.py
+++ corpus/eea_corpus.py
@@ -6,10 +6,11 @@
 # $ python -m spacy.en.download all
 
 from __future__ import unicode_literals
-import os
-import textacy
 from bs4 import BeautifulSoup
+from textacy.fileio import split_record_fields
+import os
 import phrasemachine
+import textacy
 
 CORPUS_NAME = 'eeacorpus'
 
@@ -23,18 +24,18 @@ class EEACorpus(object):
         s = 'EEA Corpus'
         return s
 
-    def create_corpus(self, fpath, normalise=False):
+    def create_corpus(self, fpath, normalize=False):
         """
         Load csv file from fpath. Each row is one document.
         It expects first column to be the Text / Body we want to analyse with
         textacy. The rest of the columns are stored as metadata associated
         to each document.
-        
-        If normalise is set to True many aspects of the text will be 
-        normalised like bad unicode, currency symbols, phone numbers, urls, 
-        emails, punctuations, accents etc. 
+
+        If normalize is set to True many aspects of the text will be
+        normalized like bad unicode, currency symbols, phone numbers, urls,
+        emails, punctuations, accents etc.
         see textacy.preprocess.preprocess_text for details.
-        
+
         Returns a textacy.Corpus.
         """
 
@@ -43,11 +44,10 @@ class EEACorpus(object):
 
         # use first column from the csv file as the text to analyse.
         # the rest is metadata
-        content_stream, metadata_stream = textacy.fileio.split_record_fields(
-            eeadocs, 0)
-            
-        if normalise:
-            content_stream = self._normalise_content_stream(content_stream)
+        content_stream, metadata_stream = split_record_fields(eeadocs, 0)
+
+        if normalize:
+            content_stream = self._normalize_content_stream(content_stream)
 
         # create textacy english Corpus
         corpus = textacy.Corpus('en', texts=content_stream,
@@ -55,7 +55,7 @@ class EEACorpus(object):
 
         return corpus
 
-    def load_or_create_corpus(self, fpath, normalise=False):
+    def load_or_create_corpus(self, fpath, normalize=False):
         base, fname = os.path.split(fpath)
         varpath = os.path.join(base, 'var')
         cpath = os.path.join(varpath, fname)
@@ -69,7 +69,7 @@ class EEACorpus(object):
             return textacy.Corpus.load(cpath, name=CORPUS_NAME)
 
         print('Creating corpus', fpath, cpath)
-        corpus = self.create_corpus(fpath, normalise)
+        corpus = self.create_corpus(fpath, normalize)
         # corpus.save(cpath, name=CORPUS_NAME)
 
         return corpus
@@ -78,41 +78,42 @@ class EEACorpus(object):
         """
         Find phrases in content via phrasemachine and return content where
         phrases are tokenized with '_' like 'air_pollution' so that they can
-        be treated as new words. 
+        be treated as new words.
         """
         # get phrases
         phrases = phrasemachine.get_phrases(content)
-        tokens = [p.replace(' ','_') for p in phrases['counts']]
+        tokens = [p.replace(' ', '_') for p in phrases['counts']]
         return tokens
 
-    def _normalise_content_stream(self, content_stream):
+    def _normalize_content_stream(self, content_stream):
         """
-        Iterate over the content, yielding one normalised content.
-        
-        Many aspects of the text will be normalised like bad unicode, 
-        currency symbols, phone numbers, urls, emails, punctuations, accents etc. 
-            
+        Iterate over the content, yielding one normalized content.
+
+        Many aspects of the text will be normalized like bad unicode, currency
+        symbols, phone numbers, urls, emails, punctuations, accents etc.
+
         Yields:
-            str: normalised plain text for the next document. 
+            str: normalized plain text for the next document.
         """
         i = 0
         for content in content_stream:
-            i +=1
+            i += 1
             if i % 100 == 0:
-                print(str(i)) # show progress in terminal
-                
+                print(str(i))   # show progress in terminal
+
             # first let us clean any html code
             soup = BeautifulSoup(content, 'html.parser')
             content = soup.get_text()
-            
+
             # then pre-proccess via textacy
-            content = textacy.preprocess.preprocess_text(content, 
-                    fix_unicode=True, lowercase=False, transliterate=True, 
-                    no_urls=True, no_emails=True, no_phone_numbers=True, 
+            content = textacy.preprocess.preprocess_text(
+                    content,
+                    fix_unicode=True, lowercase=False, transliterate=True,
+                    no_urls=True, no_emails=True, no_phone_numbers=True,
                     no_numbers=True, no_currency_symbols=True, no_punct=False,
                     no_contractions=True, no_accents=True)
-            
+
             # we are only interested in high level concepts
             content = ' '.join(self._tokenize_phrases(content))
-            
+
             yield content
